f7da350ee7fe355376a1273ca84804894b489254
lsy3709 <lsy3709@naver.com>
2022-12-02 15:34:51 +0900

    221202 6교시 저장2

--- src/ch05_test2_1202/Person.java
+++ src/ch05_test2_1202/Person.java
@@ -1,9 +1,22 @@
 package ch05_test2_1202;
 
 public class Person {
-	  String name;
+	//private : 접근 지정자, 외부에서 해당 속성을 볼수 없게 함. 
+	// 데이터 은닉 목적. 민감한 데이터는 직접 접근 불가 하게끔 만듦.
+	// get, set 함수를 이용해서 해당 데이터에 우회해서 접근함. 
+private	  String name;
 	  float height;
-	  float weight;
+	  
+	  //자동으로 생성된 set, get 함수등. 
+	  public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	float weight;
 	
 	  //기본 생성자 : 역할 : 멤버 변수를 초기화한다.
 	  // 기본 생성자는 해당 클래스 내에서 아무런 생성자가 없다면
--- src/ch05_test2_1202/PersonTest.java
+++ src/ch05_test2_1202/PersonTest.java
@@ -3,17 +3,25 @@ package ch05_test2_1202;
 public class PersonTest {
    
 	 public static void main(String[] args) {
+		 //기본 생성자 호출
 		  Person lsy = new Person();
-		  lsy.name="이상용";
+//		  lsy.name="이상용";
 		  lsy.height= 171F;
 		  lsy.weight= 75F;
+		  lsy.setName("이상용세터로추가했음.");
 		  lsy.showInfo();
+		  System.out.println("get메서드로 이름호출: "
+		  		+ lsy.getName());
+		  //set (메서드)를 이용해서 name 값 설정하기.
+		
 		  
+		  // 매개변수가 1개인 생성자를 호출. 
 		  Person lsy2 = new Person("이상용");
 		  lsy2.height= 171F;
 		  lsy2.weight= 75F;
 		  lsy2.showInfo();
 		  
+//		  매개변수가 3개인 생성자를 호출. 
 		  Person lsy3 = new Person("이상용",171F,75F);
 		  lsy3.showInfo();
 //		  personKim.name = "������";
