987f61d572987f5a6ad986d152f37cf9cf610022
lsy3709 <lsy3709@naver.com>
2022-12-05 10:27:46 +0900

    221205 2교시 저장2

--- src/ch06_sample_1019/ThisExmaple.java
+++ src/ch06_sample_1019/ThisExmaple.java
@@ -5,6 +5,12 @@ class BirthDay{
 	int month;
 	int year;
 	
+	//해당 멤버의 set/get 다 만들어서 
+	// BirthDay 의 객체를 하나 생성해보기. 참조형 변수 : ex1
+	// 매개변수가 3개인 생성자를 통해서 만들어보기.
+	// 해당 값을 출력하는 메서드 :showInfo 만들어보기.
+	// showInfo 출력 해보기.
+	
 	public void setYear(int year) {
 		this.year = year;
 	}
