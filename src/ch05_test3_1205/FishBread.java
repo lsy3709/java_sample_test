8ed893346028e6d526424e3f784a20e94c90f899
lsy3709 <lsy3709@naver.com>
2022-12-05 09:50:57 +0900

    221205 1교시 저장2

--- src/ch05_test3_1205/FishBread.java
+++ src/ch05_test3_1205/FishBread.java
@@ -10,9 +10,27 @@ public class FishBread {
 	// 예2) getPowder() 메서드로 접근. 
 	// 자동으로 해당 멤버들에 대해서 set/get 자동으로 만들었음. 
 	
+	// 기본 생성자 -> JVM이 자동으로 만들어줌. 조건) 해당 생성자가 하나도 없을 경우. 
+	// 오버로드 -> 매개 변수가 2개인 생성자를 만들기.
+	
+	//기본생성자 -> 추가로 직접 만듦.
+	public FishBread() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//매개변수가 2개인 생성자 -> 자동으로 만듦.
+	public FishBread(String powder, String redBean) {
+		super();
+		this.powder = powder;
+		this.redBean = redBean;
+	}
+	
+	
+
 	public String getPowder() {
 		return powder;
 	}
+	
 	public void setPowder(String powder) {
 		this.powder = powder;
 	}
