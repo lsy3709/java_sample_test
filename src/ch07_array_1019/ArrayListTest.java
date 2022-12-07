14a13acd6bf2ed0fad344bd174ea5bde624ddee0
lsy3709 <lsy3709@naver.com>
2022-12-07 12:09:57 +0900

    221207 4교시 저장1

--- src/ch07_array_1019/ArrayListTest.java
+++ src/ch07_array_1019/ArrayListTest.java
@@ -7,16 +7,26 @@ public class ArrayListTest {
 
 	public static void main(String[] args) {
 
+		//Student 타입으로 ArrayList 를 만들기. -> students 
+		// 해당 ArrayList에 각자 알고 있는 이름 3개만 등록하기.
+		// Student에 생성자를 잘 보고 객체 만들기.
+		// showInfo를 이용해 출력해보기.
+		// 기본 for문 출력하고
+		// 향상된 for 문 출력하기. 
+		
+		//기본 선언하는 부분
 		ArrayList<Book> library = new ArrayList<Book>();
 
-		// Book 타입만 넣기
+		// Book 타입만 넣기 <Book> 제너릭 타입형식으로 지정. 
 		//library.add("lsy");
+		// library , ArrayList 형식은 Book 타입의 객체 5개를 등록 했음. 
 		library.add( new Book("태백산맥", "조정래") );
 		library.add( new Book("데미안", "헤르만 헤세") );
 		library.add( new Book("어떻게 살 것인가", "유시민") );
 		library.add( new Book("토지", "박경리") );
 		library.add( new Book("어린왕자", "생텍쥐페리") );
 		
+		//기본 반복문 for를 이용해서 , 해당 ArrayList 에 있는 값을 가져오기.
 		for(int i=0; i<library.size(); i++){
 	
 			Book book = library.get(i);
