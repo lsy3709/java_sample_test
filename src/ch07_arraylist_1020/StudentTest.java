522f2e8314ba70391cd3911fa3e007c7e533e74d
lsy3709 <lsy3709@naver.com>
2022-12-07 12:36:49 +0900

    221207 4교시 저장2

--- src/ch07_arraylist_1020/StudentTest.java
+++ src/ch07_arraylist_1020/StudentTest.java
@@ -4,8 +4,20 @@ package ch07_arraylist_1020;
 public class StudentTest {
 
 	public static void main(String[] args) {
+		// 본인 이름의 Student 형으로 객체를 생성합니다. 학번은 임의로 
+		// 본인 수강하고 싶은 과목 3개를 등록하기(점수 포함). 예) java, spring, Android
+		// 해당 정보 출력하기.showStudentInfo  활용하기. 
+		
+		// LunchMenu : ArrayList 만들어서, Student 클래스에 추가해서,
+		// 점심 메뉴 추가하는 기능, 출력하는 기능등을 만들어서 , 출력해보기.
+		// 참고는 Subject를 참고해서 만들어보기.
+		
+		
+		//Student 형으로 객체를 생성하면
+		// 멤버로 , 아이디, 이름, 과목 리스트 구성이됩니다.
 		Student studentLee = new Student(1001, "Lee");
 		
+		// 해당 과목 리스트에 과목을 추가하기. 객체를 추가한 것.
 		studentLee.addSubject("국어", 100);
 		studentLee.addSubject("수학", 50);
 		
@@ -23,9 +35,9 @@ public class StudentTest {
 		studentLsy.addSubject("수학", 85);
 		studentLsy.addSubject("영어", 100);
 		
-		studentLee.showStudentInfo();
-		System.out.println("======================================");
-		studentKim.showStudentInfo();
+//		studentLee.showStudentInfo();
+//		System.out.println("======================================");
+//		studentKim.showStudentInfo();
 		
 		System.out.println("======================================");
 		studentLsy.showStudentInfo();
