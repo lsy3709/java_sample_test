94ee2f0c9a382cba7d0420c3baf2a9d28b14dc07
lsy3709 <lsy3709@naver.com>
2022-12-09 10:32:43 +0900

    221209 2교시 저장1

--- src/ch10_interfaceex_1021/Customer.java
+++ src/ch10_interfaceex_1021/Customer.java
@@ -4,18 +4,24 @@ public class Customer implements Buy, Sell{
 
 	@Override
 	public void buy() {
-		System.out.println("구매하기");
+		System.out.println("Customer 구매하기");
 	}
 
 	@Override
 	public void sell() {
-		System.out.println("판매하기");
+		System.out.println("Customer 판매하기");
 	}
 
 	@Override
 	public void order() {
-		System.out.println("고객 판매 주문");
+		System.out.println("Customer order 고객 판매 주문");
 	}
 }
+
+
+
+
+
+
 
 
