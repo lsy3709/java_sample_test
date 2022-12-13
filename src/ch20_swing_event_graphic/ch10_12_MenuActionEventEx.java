2802395526043a2ca2e33d8e4ffb788030f3c4fd
lsy3709 <lsy3709@naver.com>
2022-12-14 08:56:33 +0900

    221214 스윙,이벤트,그래픽 예제 추가 중2

---
+++ src/ch20_swing_event_graphic/ch10_12_MenuActionEventEx.java
@@ -0,0 +1,73 @@
+package ch20_swing_event_graphic;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class ch10_12_MenuActionEventEx extends JFrame {
+
+	private JLabel imgLabel = new JLabel();
+	
+	public ch10_12_MenuActionEventEx() {
+		setTitle(" 메뉴 및 이벤트 예제");
+		createMenu();
+		getContentPane().add(imgLabel,BorderLayout.CENTER);
+		setSize(500,400);
+		setVisible(true);
+	}
+	
+	public void createMenu() {
+		JMenuBar mb = new JMenuBar();
+		JMenuItem [] menuItem = new JMenuItem[4];
+		String[] itemTitle = {"Load", "Hide", "ReShow", "Exit"};
+		JMenu screenMenu = new JMenu("Screen");
+		
+		MenuActionListener listener = new MenuActionListener();
+		
+		for(int i=0; i<menuItem.length; i++) {
+			menuItem[i] = new JMenuItem(itemTitle[i]);
+			menuItem[i].addActionListener(listener);
+			screenMenu.add(menuItem[i]);
+		}
+		mb.add(screenMenu);
+		setJMenuBar(mb);
+		
+	}
+	
+	class MenuActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String cmd = e.getActionCommand();
+			switch(cmd) {
+			case "Load":
+				if(imgLabel.getIcon() != null) return;
+				imgLabel.setIcon(new ImageIcon("image/라바1.png"));
+				break;
+			case "Hide":
+				imgLabel.setVisible(false);
+				break;
+			case "ReShow":
+				imgLabel.setVisible(true);
+				break;
+			case "Exit":
+				System.exit(0); 
+				break;
+				
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		new ch10_12_MenuActionEventEx();
+	}
+
+}
