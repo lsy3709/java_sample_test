c2db1f5a975b8c1eef85a418d2a0fe99f8d11449
lsy3709 <lsy3709@naver.com>
2022-12-15 08:56:51 +0900

    221215 그래픽 응용 테스트 중 1

--- src/ch20_swing_event_graphic/ch11_9_GraphicsDrawLineMouseEx3.java
+++ src/ch20_swing_event_graphic/ch11_9_GraphicsDrawLineMouseEx3.java
@@ -1,31 +1,42 @@
 package ch20_swing_event_graphic;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class ch11_9_GraphicsDrawLineMouseEx3 extends JFrame {
 	public ch11_9_GraphicsDrawLineMouseEx3() {
 		setTitle("버튼 클릭, 선,타원, 사각형,그리기 응용 예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setContentPane(new MyPanel());
+		MyPanel panel1 = new MyPanel();
+		setContentPane(panel1);
 		setSize(300, 300);
 		setVisible(true);
 	}
-
-	public static void main(String [] args) {
-		new ch11_9_GraphicsDrawLineMouseEx3();
-	} 
-
 	class MyPanel extends JPanel {
 		private Vector<Point> vStart = new Vector<Point>();
 		private Vector<Point> vEnd = new Vector<Point>();
+		
+		private Vector<Point> vStart2 = new Vector<Point>();
+		private Vector<Point> vEnd2 = new Vector<Point>();
+		
 		int state = 0;
 		
+		JButton btn1 = new JButton("선");
+		JButton btn2 = new JButton("타원");
+		
 		public MyPanel() {
-			JButton btn1 = new JButton("선");
-			JButton btn2 = new JButton("타원");
+		
 			add(btn1);
 			add(btn2);
 			
@@ -33,11 +44,14 @@ public class ch11_9_GraphicsDrawLineMouseEx3 extends JFrame {
 				
 				@Override
 				public void actionPerformed(ActionEvent e) {
+					
+					repaint();
 					addMouseListener(new MouseAdapter(){
 						public void mousePressed(MouseEvent e) {
+							state = 0;
 							Point startP = e.getPoint();
 							vStart.add(startP);
-							state = 0;
+							
 						}
 						public void mouseReleased(MouseEvent e) {
 							Point endP = e.getPoint();
@@ -57,13 +71,13 @@ public class ch11_9_GraphicsDrawLineMouseEx3 extends JFrame {
 				public void actionPerformed(ActionEvent e) {
 					addMouseListener(new MouseAdapter(){
 						public void mousePressed(MouseEvent e) {
-							Point startP = e.getPoint();
-							vStart.add(startP);
 							state = 1;
+							Point startP2 = e.getPoint();
+							vStart2.add(startP2);
 						}
 						public void mouseReleased(MouseEvent e) {
-							Point endP = e.getPoint();
-							vEnd.add(endP);
+							Point endP2 = e.getPoint();
+							vEnd2.add(endP2);
 							
 							// 패널의 다시 그리기를 요청한다.
 							repaint(); // 주목
@@ -81,21 +95,44 @@ public class ch11_9_GraphicsDrawLineMouseEx3 extends JFrame {
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			g.setColor(Color.BLUE); 
+			if(state == 0) {
 			for(int i=0; i<vStart.size(); i++) {
 				Point s = vStart.elementAt(i); 
 				Point e = vEnd.elementAt(i);
-				if(state == 0) {
+				
 				g.drawLine((int)s.getX(), (int)s.getY(), 
 								(int)e.getX(), (int)e.getY());
-				}
+				
+				
+				} 
+			
+			for(int i=0; i<vStart2.size(); i++) {
+				Point s2 = vStart2.elementAt(i); 
+				Point e2 = vEnd2.elementAt(i);
+			int x = Math.min((int)s2.getX(), (int)e2.getX());
+		int y = Math.min((int)s2.getY(), (int)e2.getY());
+		int width = Math.abs((int)s2.getX() - (int)e2.getX());
+		int height = Math.abs((int)s2.getY() - (int)e2.getY());
+		g.drawOval(x, y, width, height); // 타원 그리기
+	}
+			
+			}
 				else {
-					int x = Math.min((int)s.getX(), (int)e.getX());
-				int y = Math.min((int)s.getY(), (int)e.getY());
-				int width = Math.abs((int)s.getX() - (int)e.getX());
-				int height = Math.abs((int)s.getY() - (int)e.getY());
+					for(int i=0; i<vStart2.size(); i++) {
+						Point s2 = vStart2.elementAt(i); 
+						Point e2 = vEnd2.elementAt(i);
+					int x = Math.min((int)s2.getX(), (int)e2.getX());
+				int y = Math.min((int)s2.getY(), (int)e2.getY());
+				int width = Math.abs((int)s2.getX() - (int)e2.getX());
+				int height = Math.abs((int)s2.getY() - (int)e2.getY());
 				g.drawOval(x, y, width, height); // 타원 그리기
 			}
 		}
 	}
 }
+	public static void main(String [] args) {
+		new ch11_9_GraphicsDrawLineMouseEx3();
+	} 
+
+
 }
